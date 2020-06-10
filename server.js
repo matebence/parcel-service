7e44c0426a1f21f09ae19f735afb08e1cb80e083
Bence <m.bence05@gmail.com>
2020-06-10 11:24:05 +0200

    pripojenie ku config server

---
+++ server.js
@@ -0,0 +1,24 @@
+const node = require('./resources/bootstrap');
+const client = require("cloud-config-client");
+const bodyParser = require('body-parser');
+const express = require('express');
+const helmet = require('helmet');
+const cors = require("cors");
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+
+client.load({
+    endpoint: node.cloud.config.uri,
+    name: node.application.name,
+    profiles: node.profiles.active,
+    auth: {user: node.cloud.config.username, pass: node.cloud.config.password}
+}).then(config => {
+    config.bootstrap = node;
+    return app.listen(node.server.port);
+}).then(() => {
+    console.log(`Server beží na porte ${node.server.port}`)
+}).catch(console.error);
